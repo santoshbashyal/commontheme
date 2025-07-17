e780c3cde9b917b99b3e7193673df6e5a8e32243
Santosh Bashyal <santoshbashyal000@gmail.com>
2025-07-17 17:53:27 +0545

    fix error

--- src/pages/users/list-users/users-list.tsx
+++ src/pages/users/list-users/users-list.tsx
@@ -1,14 +1,9 @@
 import { userListColumns, userListData } from "./userListData";
-import type { UserRow } from "./types";
 import ListPage from "../../../components/list/ListPage";
 
 const UserListPage = () => {
   return (
-    <ListPage<UserRow>
-      title="User List"
-      columns={userListColumns}
-      data={userListData}
-    />
+    <ListPage title="User List" columns={userListColumns} data={userListData} />
   );
 };
 
