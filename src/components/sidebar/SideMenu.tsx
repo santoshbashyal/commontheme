05b820d5e2f661c635c3afa0674e4c3d2b9143a1
Santosh Bashyal <santoshbashyal000@gmail.com>
2025-08-24 18:09:45 +0545

    fix: Dashboard bold when active

--- src/components/sidebar/SideMenu.tsx
+++ src/components/sidebar/SideMenu.tsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router";
 import ExpandableTab from "./ExpandableTab";
 import ImageName from "./ImageName";
 import { SideMenuData } from "./SideMenuData";
@@ -8,12 +9,24 @@ const SideMenu = () => {
       <div className=" w-[212px] p-4 ">
         <ImageName />
         <div className="flex flex-col mt-4">
-          <div className="flex  p-2 rounded-2xl  items-center my-1 gap-2 ">
+          {/* Dashboard tab */}
+
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              `flex  p-2 rounded-2xl  items-center my-1 gap-2 ${
+                isActive
+                  ? "bg-gray-200 text-gray-800 font-bold"
+                  : "hover:bg-gray-100"
+              }`
+            }
+          >
             <div>
-              <img src="settings.svg" alt="" />
+              <img src="/settings.svg" alt="" />
             </div>
             <div className="px-4">Dashboard</div>
-          </div>
+          </NavLink>
+
           {SideMenuData.map((menuItem, index) => (
             <ExpandableTab
               key={index}
